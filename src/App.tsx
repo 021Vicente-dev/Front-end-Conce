fc5c5860b09890d835232e04147ce2cb226f4eb4
Ryan Vicente <vicente.dev021@gmail.com>
2024-09-03 17:50:47 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import { Vehicle } from './types/types';
 
 const vehicles: Vehicle[] = [
   //Carros
-  { id: 1, name: "Adder", model: "adder", price: 1000000, img: '../public/img_carros/adder.png', category: 'carro', peso: 50 },
-  { id: 2, name: "Zentorno", model: "zentorno", price: 900000, img: '../public/img_carros/zentorno.png', category: 'carro', peso: 50 },
-  { id: 3, name: "Lambo", model: "Lambo", price: 300000, img: "../public/img_carros/lambo.png", category: 'carro', peso: 50 },
+  { id: 1, name: "Adder", model: "adder", price: 1000000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280629893872484506/image.png?ex=66d8c707&is=66d77587&hm=8a9c27ba5a49d54ef333e7c537c2b63ecb359b4a936bcbe51be5d7b5f334e82b&', category: 'carro', peso: 50 },
+  { id: 2, name: "Zentorno", model: "zentorno", price: 900000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280629929381203989/image.png?ex=66d8c710&is=66d77590&hm=28bb1a4688bc8efdc39f3cf43065780056264d7d5572d674f7edfee093e87055&', category: 'carro', peso: 50 },
+  { id: 3, name: "Lambo", model: "Lambo", price: 300000, img: "https://cdn.discordapp.com/attachments/1172335701874974822/1280629910230011924/image.png?ex=66d8c70b&is=66d7758b&hm=93af2c5fa3231b40ccf78aa771cbd3f55eaa346730dfa3d266a2f2573d1084d9&", category: 'carro', peso: 50 },
   //bicicleta
-  { id: 4, name: "Bike", model: "bike", price: 50000, img: '../public/img_bikes/bike1.png', category: 'bicicleta', peso: 0 },
-  { id: 5, name: "Bike", model: "bike", price: 50000, img: '../public/img_bikes/bike2.png', category: 'bicicleta', peso: 0 },
-  { id: 6, name: "Bike", model: "bike", price: 50000, img: '../public/img_bikes/bike3.png', category: 'bicicleta', peso: 0 },
+  { id: 4, name: "Bike", model: "bike", price: 50000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280629110879686746/image.png?ex=66d8c64c&is=66d774cc&hm=c431b39700910d9725cf21d7686a9b926d67489521f5add9e47bf2a5de5962e4&', category: 'bicicleta', peso: 0 },
+  { id: 5, name: "Bike", model: "bike", price: 50000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280629090201636905/image.png?ex=66d8c648&is=66d774c8&hm=a661f6d3fac73588c48025171fb153ace128c28d33b3075b78efff591dda5f48&', category: 'bicicleta', peso: 0 },
+  { id: 6, name: "Bike", model: "bike", price: 50000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280629068932317224/image.png?ex=66d8c642&is=66d774c2&hm=41654cf0daef39b121867784ff1bf8495dcd0b0b545fe11bdc7be09c53fbd206&', category: 'bicicleta', peso: 0 },
   //Motos
-  { id: 7, name: "CG Fan 160", model: "CITY", price: 15000, img: '../public/img_motos/fan160.png', category: 'moto', peso: 20 },
-  { id: 8, name: "Pop 100", model: "CITY", price: 15000, img: '../public/img_motos/pop100.png', category: 'moto', peso: 20 },
-  { id: 9, name: "R150 GS", model: "Adventure", price: 15000, img: '../public/img_motos/r1250.png', category: 'moto', peso: 20 },
+  { id: 7, name: "CG Fan 160", model: "CITY", price: 15000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630861917589514/image.png?ex=66d8c7ee&is=66d7766e&hm=7ed02f84773777ae93e1afe844c20707138295be6ba88be6c1de2c59f2912c79&', category: 'moto', peso: 20 },
+  { id: 8, name: "Pop 100", model: "CITY", price: 15000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630877260615820/image.png?ex=66d8c7f2&is=66d77672&hm=c05ecb1f55ff10151ac4f6737ff0baf9c1380f64978dad586930f9d71b678d9b&', category: 'moto', peso: 20 },
+  { id: 9, name: "R150 GS", model: "Adventure", price: 15000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630908524826761/image.png?ex=66d8c7f9&is=66d77679&hm=c1cbe9983a606cf96c93993c531e0c4e211005fc0ed4504d9b05fcbf920b7c81&', category: 'moto', peso: 20 },
   //Caminhões
-  { id: 10, name: "Volvo Fh", model: "Cabine Globetrotter XL", price: 200000, img: '../public/img_caminhao/caminhao1.png', category: 'caminhão', peso: 500 },
-  { id: 11, name: "VWCO", model: "Metor", price: 200000, img: '../public/img_caminhao/caminhao2.png', category: 'caminhão', peso: 500 },
-  { id: 12, name: "Caminhão", model: "açougue", price: 200000, img: '../public/img_caminhao/caminhao3.png', category: 'caminhão', peso: 500 },
+  { id: 10, name: "Volvo Fh", model: "Cabine Globetrotter XL", price: 200000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630109908369510/image.png?ex=66d8c73b&is=66d775bb&hm=24e421c0e07625e6605de40f023e1c511667eac7880dcfea2d3f0791c6504156&', category: 'caminhão', peso: 500 },
+  { id: 11, name: "VWCO", model: "Metor", price: 200000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630408916242454/image.png?ex=66d8c782&is=66d77602&hm=b3cbe4fd63211f16bdf6344d1989582336e09f13c4189af23ab670b02e008120&', category: 'caminhão', peso: 500 },
+  { id: 12, name: "Caminhão", model: "açougue", price: 200000, img: 'https://cdn.discordapp.com/attachments/1172335701874974822/1280630452834799666/image.png?ex=66d8c78c&is=66d7760c&hm=944c8b41070dfb82c58c6ee7ec85e493dff7ad36f84e6efb33de0d3ee9644123&', category: 'caminhão', peso: 500 },
 ];
 
 const App: React.FC = () => {
